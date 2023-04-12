f844f700c0f407c143929bb38dc7ce333452c8cd
ViktoriaGavrosh <viktoria.gavrosh@gmail.com>
2023-04-12 16:22:19 +0300

    start game

---
+++ Connect Four/task/src/connectfour/Main.kt
@@ -0,0 +1,52 @@
+package connectfour
+
+fun main() {
+    start()
+}
+
+fun start() {
+    println("Connect Four")
+    println("First player's name:")
+    val firstPlayer = readln()
+    println("Second player's name:")
+    val secondPlayer = readln()
+
+    val text = buildBoard()
+    printStartGame(firstPlayer, secondPlayer, text)
+}
+
+fun buildBoard(): String {
+    var result: String
+    while (true) {
+        println("Set the board dimensions (Rows x Columns)\nPress Enter for default (6 x 7)")
+        result = readln()
+        if (result == "") {
+            result = "6X7"
+            break
+        }
+        val res = result.replace(" ", "").uppercase()
+        when {
+            !Regex(".X.").matches(res) -> {
+                println("Invalid input")
+                continue
+            }
+            !Regex("[5-9]..").matches(res) -> {
+                println("Board rows should be from 5 to 9")
+                continue
+            }
+            !Regex("..[5-9]").matches(res) -> {
+                println("Board columns should be from 5 to 9")
+                continue
+            }
+            else -> {
+                result = result.trim()
+                break
+            }
+        }
+    }
+    return result
+}
+
+fun printStartGame(firstPlayer: String, secondPlayer: String, text: String) {
+    println("$firstPlayer VS $secondPlayer\n${text[0]} X ${text[2]} board")
+}
