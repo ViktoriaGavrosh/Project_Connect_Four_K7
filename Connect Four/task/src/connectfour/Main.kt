1664f33a57472173764b2a58a63755c7fc08bde0
ViktoriaGavrosh <viktoria.gavrosh@gmail.com>
2023-04-18 19:05:39 +0300

    ok

--- Connect Four/task/src/connectfour/Main.kt
+++ Connect Four/task/src/connectfour/Main.kt
@@ -38,22 +38,18 @@ class Main {
                     println("Invalid input")
                     continue
                 }
-
                 res.indexOf('X') != 1 || !Regex("[5-9][\\s\\S]*").matches(res) -> {
                     println("Board rows should be from 5 to 9")
                     continue
                 }
-
                 res.length > 3 || !Regex("[\\s\\S]*[5-9]").matches(res) -> {
                     println("Board columns should be from 5 to 9")
                     continue
                 }
-
                 !Regex(".X.").matches(res) -> {
                     println("Invalid input")
                     continue
                 }
-
                 else -> {
                     result = res
                     break
